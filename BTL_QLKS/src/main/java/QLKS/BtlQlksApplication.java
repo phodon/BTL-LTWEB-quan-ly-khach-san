7acc34127ed01cd423ad2106afa223eb29b89446
batchanhdao <92378282+batchanhdao@users.noreply.github.com>
2023-04-15 10:56:02 +0700

    anhgdt

---
+++ BTL_QLKS/src/main/java/QLKS/BtlQlksApplication.java
@@ -0,0 +1,13 @@
+package QLKS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlQlksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlQlksApplication.class, args);
+	}
+
+}
---
+++ BTL_QLKS/src/test/java/QLKS/BtlQlksApplicationTests.java
@@ -0,0 +1,13 @@
+package QLKS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlQlksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
